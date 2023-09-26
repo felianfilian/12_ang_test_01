1bb8c34b28ed737df806929dae83cfb0b6661cd7
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-09-26 23:49:50 +0200

    first logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ang_test_01';
+export class AppComponent implements OnInit {
+  images = ['img01.jpg', 'img02.jpg', 'img03.jpg'];
+
+  ngOnInit() {}
+
+  updateImage() {}
 }
