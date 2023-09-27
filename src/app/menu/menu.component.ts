8160a9cde35f48c9cb3947c1e973c0e65019132e
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-09-27 23:33:06 +0200

    set variable for dark mode

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent {
-
+  @Input() darkMode = true;
 }
