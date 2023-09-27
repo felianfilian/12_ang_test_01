df71cb4af9ee69e39824406c98dbd1247953a836
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-09-28 01:45:46 +0200

    data prot page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { DataProtectionComponent } from './data-protection/data-protection.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'imprint',
     component: ImprintComponent,
+  },
+  {
+    path: 'data-protection',
+    component: DataProtectionComponent,
   },
 ];
 
